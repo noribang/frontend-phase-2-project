43386632276e756e4bfad15b547a74dfb60c44f6
hoonkim <hoonk9387@gmail.com>
2022-06-26 21:38:25 -0500

    Simple counter.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 // import logo from './logo.svg';
+import { useState }  from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
